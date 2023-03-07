b0ce83a32b6c81b8d406f51a787f2e0c8d8dc61d
Joe Stubbs <joestubbs5@gmail.com>
2023-03-07 07:16:42 -0600

    Remove call to batch detector

--- external_plugins/image_scoring_plugin/entrypoint.sh
+++ external_plugins/image_scoring_plugin/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-python /CameraTraps/detection/run_detector_batch.py md_v5a.0.0.pt "$images_dir" "$output_file_path" --recursive --output_relative_filenames --quiet
-
 python image_scoring_plugin.py
