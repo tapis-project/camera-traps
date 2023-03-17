53d90828b63f654ea00c8e11ad71998753f991b7
Joe Stubbs <joestubbs5@gmail.com>
2023-03-17 16:08:28 -0500

    revert stripping of ' character from image file name, as the stripping seems to be required to prevent IO errors reading the file.

--- external_plugins/image_generating_plugin/image_generating_plugin.py
+++ external_plugins/image_generating_plugin/image_generating_plugin.py
@@ -28,9 +28,9 @@ start = int(data['timestamp'])
 
 def get_binary(value):
     uuid_image = str(uuid.uuid5(uuid.NAMESPACE_URL, value))
-    with open(str(value), "rb") as f:
+    with open(str(value)[1:-1], "rb") as f:
         binary_img = f.read()
-    img = Image.open(str(value))
+    img = Image.open(str(value)[1:-1])
     img_format = img.format
     print(f"sending new image with the following data; uuid:{uuid_image}; format: {img_format}; type(format): {type(img_format)}")
     ctevents.send_new_image_fb_event(
