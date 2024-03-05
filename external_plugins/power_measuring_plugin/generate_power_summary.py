97a4e39032520e38925e980df084d04954f1b01c
serenashah <serena.shah@utexas.edu>
2024-03-05 17:52:33 -0600

    power sum function

--- external_plugins/power_measuring_plugin/generate_power_summary.py
+++ external_plugins/power_measuring_plugin/generate_power_summary.py
@@ -1,22 +1,92 @@
 from jsonschema import validate
 import json
+from datetime import datetime
 
 def generate_summary():
-    # validate 
-    validate_metadata_schema("example_metadata.json")
-    # validate_log_schema("example_cpu.json")
-
-def validate_metadata_schema(metadata_path):
-    """
-    Validate incoming metadata json file is of correct form
-    """
     # open metadata file
     try:
-        with open(metadata_path, 'r') as file:
-            metadata_file = json.load(file)
+        with open("example_metadata.json", 'r') as file:
+            metadata = json.load(file)
     except FileNotFoundError:
         exit()
 
+    # open cpu file
+    try:
+        with open("example_cpu.json", 'r') as file:
+            cpu_log = json.load(file)
+    except FileNotFoundError:
+        exit()
+
+    # validate 
+    validate_metadata_schema(metadata)
+    validate_log_schema(cpu_log)
+
+    # initialize summary list
+    summary = summary_init(metadata)
+
+    # start and end time
+    monitor_times(cpu_log, summary)
+
+    # sum power consumption
+    sum_power_consumption(cpu_log, summary, 'cpu_consumption')
+ 
+    # write to json file
+    with open("sample_report.json", "w") as outfile: 
+        json.dump(summary, outfile, indent=2)
+
+def sum_power_consumption(log, summary, device):
+    """
+    sum power log, given log list of dicts, summary list of dicts, device string
+    """
+    for i, report in enumerate(summary):
+        summary[i][device] = 0
+        for entry in log:
+            logs_at_time = (list(entry.values())[0]) # [[0.0, '2437322'], [1.4, '3423844'], [2.3, '4737228']]
+            for j in logs_at_time:
+                # if pid in log matches summary, increment power value
+                if int(j[1]) == summary[i]['pid']:
+                    summary[i][device] += j[0]
+
+    return summary
+    
+def summary_init(metadata):
+    """
+    initialize summary report
+    """
+    summary = []
+    for plugin in metadata['plugins']:
+        for pid in plugin['pids']:
+            summary_dict = {"pid": None, "command_line": None, 
+                "start_time": None, "end_time": None, 
+                "cpu_consumption": None, "gpu_consumption": None}
+            summary_dict["pid"] = pid
+            summary_dict["command_line"] = plugin['command_line']
+            summary.append(summary_dict)
+
+    return summary
+
+def monitor_times(log, summary):
+    """
+    find start and end time
+    """
+    all_times = []
+    starts = []
+    ends = []
+    # for i, report in enumerate(summary):
+    #     for entry in log:
+    #         datetime_object = datetime.strptime(list(entry.keys())[0], "%m/%d/%Y %I:%M:%S %p")
+    #         all_times.append(datetime_object)
+    #     start_time = min(all_times)
+    #     end_time = max(all_times)
+
+
+
+    print(start_time)
+
+def validate_metadata_schema(metadata_file):
+    """
+    Validate incoming metadata json file is of correct form
+    """
     # set schema
     metadata_schema = {
         "$schema": "http://json-schema.org/schema#",
@@ -32,7 +102,8 @@ def validate_metadata_schema(metadata_path):
                             "enum": [
                                     "image_scoring_plugin",
                                     "image_generating_plugin",
-                                    "power_generating_plugin"
+                                    "power_generating_plugin",
+                                    "engine"
                                 ]
                             },
                         "pids": {
@@ -53,7 +124,6 @@ def validate_metadata_schema(metadata_path):
                         },
                         "command_line":{
                             "type": "string"
-                            # any way to specify more?
                         }
                     },
                     "required": ["name", "pids", "devices_measured", "command_line"]          
@@ -74,15 +144,17 @@ def validate_metadata_schema(metadata_path):
                             },
                             "tool_name": {
                                 "type": "string",
-                                # enum?
+                                "enum": [
+                                    "jtop",
+                                    "scaph",
+                                    "nvsmi"
+                                ]
                             },
                             "tool_params": {
                                 "type": "string",
-                                # enum?
                             },
                             "power_units": {
                                 "type": "string",
-                                # include enum or no?
                                 "enum": [
                                     "watts"
                                 ]
@@ -108,17 +180,10 @@ def validate_metadata_schema(metadata_path):
         print("Metadata validation failed:", e)
         exit()
 
-def validate_log_schema(log_path):
+def validate_log_schema(log_file):
     """
     Validate incoming log json file is of correct form
     """
-    # open log file
-    try:
-        with open(log_path, 'r') as file:
-            log_file = json.load(file)
-    except FileNotFoundError:
-        exit()
-
     # set log schema
     log_schema = {
         "$schema": "http://json-schema.org/schema#",
@@ -158,5 +223,4 @@ def validate_log_schema(log_path):
         exit()
 
 if __name__ == "__main__":
-    validate_metadata_schema("example_metadata.json")
-    validate_log_schema("example_cpu.json")
+    generate_summary()
