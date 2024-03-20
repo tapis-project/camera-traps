e3804f9bbd495b522df1a01cb263e03607a88b5e
serenashah <serena.shah@utexas.edu>
2024-03-20 12:13:08 -0500

    validate schemas script

--- external_plugins/power_measuring_plugin/generate_power_summary.py
+++ external_plugins/power_measuring_plugin/generate_power_summary.py
@@ -1,21 +1,23 @@
-from jsonschema import validate
-import json
 from datetime import datetime
+import json
+from validate_schemas import validate_log_schema, validate_metadata_schema
 
 def generate_power_summary():
     # open metadata file
     try:
-        with open("example_metadata.json", 'r') as file:
+        with open("metadata.json", 'r') as file:
             metadata = json.load(file)
     except FileNotFoundError:
+        print("metadata.json not found")
         exit()
 
     # open cpu file
     try:
-        with open("example_cpu.json", 'r') as file:
+        with open("cpu.json", 'r') as file:
             cpu_log = json.load(file)
     except FileNotFoundError:
-        exit()
+        print("cpu.json not found")
+        exit()  
 
     # validate 
     validate_metadata_schema(metadata)
@@ -32,6 +34,7 @@ def generate_power_summary():
  
     # write to json file
     with open("power_summary_report.json", "w") as outfile: 
+        print("Writing to power_summary_report.json")
         json.dump(summary, outfile, indent=2)
 
 def sum_power_consumption(log, summary, device):
@@ -90,146 +93,6 @@ def monitor_times(log, summary):
                         if summary_endtime_datetime < log_datetime:
                             report['end_time'] = time
                         
-
-
-def validate_metadata_schema(metadata_file):
-    """
-    Validate incoming metadata json file is of correct form
-    """
-    # set schema
-    metadata_schema = {
-        "$schema": "http://json-schema.org/schema#",
-        "type": "object",
-        "properties": {
-            "plugins": {
-                "type":"array", 
-                "items": {
-                    "type":"object",
-                    "properties": {
-                        "name": {
-                            "type": "string",
-                            "enum": [
-                                    "image_scoring_plugin",
-                                    "image_generating_plugin",
-                                    "power_generating_plugin",
-                                    "engine"
-                                ]
-                            },
-                        "pids": {
-                            "type": "array", 
-                            "items": {"type":"integer"},
-                            "minItems": 1,
-                            "uniqueItems": True
-                            },
-                        "devices_measured": {
-                            "type": "array",
-                            "items": {
-                                "type":"string", 
-                                "enum": [
-                                    "gpu",
-                                    "cpu"
-                                ]
-                            }
-                        },
-                        "command_line":{
-                            "type": "string"
-                        }
-                    },
-                    "required": ["name", "pids", "devices_measured", "command_line"]          
-                }
-            },
-            "tools":{
-                "type": "object",
-                "properties": {
-                    "devices": {
-                        "type": "object",
-                        "properties": {
-                            "device_type": {
-                                "type": "string",
-                                "enum": [
-                                    "gpu",
-                                    "cpu"
-                                ]
-                            },
-                            "tool_name": {
-                                "type": "string",
-                                "enum": [
-                                    "jtop",
-                                    "scaph",
-                                    "nvsmi"
-                                ]
-                            },
-                            "tool_params": {
-                                "type": "string",
-                            },
-                            "power_units": {
-                                "type": "string",
-                                "enum": [
-                                    "watts"
-                                ]
-                            }
-                        },
-                        "required": ["device_type", "tool_name", "tool_params", "power_units"]
-                    }
-                },
-                "required": ["devices"]
-            },
-            "start_time": {
-                "type": "string",
-                "format": "date-time"
-            }
-        },
-        "required": ["plugins", "tools", "start_time"]
-    }
-
-    # validate schema
-    try: 
-        validate(instance=metadata_file, schema=metadata_schema)
-    except Exception as e:
-        print("Metadata validation failed:", e)
-        exit()
-
-def validate_log_schema(log_file):
-    """
-    Validate incoming log json file is of correct form
-    """
-    # set log schema
-    log_schema = {
-        "$schema": "http://json-schema.org/schema#",
-        "type": "object",
-        "properties": {
-            "logs": {
-                "type": "array",
-                "items": {
-                    "type": "object",
-                    "patternProperties": {
-                        "^\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2} (AM|PM)$": {
-                            "type": "array",
-                            "items": {
-                                "type": "array",
-                                "items": [
-                                    {"type": "number"},
-                                    {"type": "string"}
-                                ],
-                                "minItems": 2,
-                                "maxItems": 2
-                            }
-                        }
-                    }
-                }
-            }   
-        }
-    }
-
-    # new schema to avoid top level array error
-    log_schema_object = {"logs": log_file}
-
-    # validate schema
-    try: 
-        validate(instance=log_file, schema=log_schema_object)
-    except Exception as e:
-        print("Log validation failed:", e)
-        exit()
 
 if __name__ == "__main__":
     generate_power_summary()
