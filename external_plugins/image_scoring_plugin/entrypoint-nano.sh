8bb97013a0a3f7b1c900722559ed2ecd422806fe
sowbaranika1302 <balasubramaniam.29@osu.edu>
2024-08-02 05:07:22 +0000

    Modified the bash script

--- external_plugins/image_scoring_plugin/entrypoint-nano.sh
+++ external_plugins/image_scoring_plugin/entrypoint-nano.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo "Start of Image Scoring Plugin Nano..."
-if [ "$MODEL_TYPE" -eq 1 ]; then
+if [ $MODEL_TYPE -eq 1 ]; then
   wget -q -O /md_v5a.0.0.pt https://github.com/ICICLE-ai/camera_traps/raw/main/models/md_v5b.0.0.pt
   echo "Executing the MDV5B second model. Downloading the model..."
 fi
 
-if [ "$MODEL_TYPE" -eq 2 ]; then
+if [ $MODEL_TYPE -eq 2 ]; then
   wget -q -O /md_v5a.0.0.pt https://github.com/ICICLE-ai/camera_traps/raw/main/models/md_v5a.0.0_ena.pt
   echo "Executing the Megadetector fine-tuned ENA model. Downloading the model..."
 fi
 
-if [ "$MODEL_TYPE" -eq 3 ]; then
+if [ $MODEL_TYPE -eq 3 ]; then
   wget -q -O /md_v5a.0.0.pt https://github.com/ICICLE-ai/camera_traps/raw/main/models/mdv5_optimized.pt
   echo "Executing the Megadetector optimized model. Downloading the model..."
 fi
