5e05df576bf20267c778e00587ca0e3348b6fdcf
Joe Stubbs <joestubbs5@gmail.com>
2024-09-15 18:37:47 -0500

    Fix race conditions and clean up code in image generating and oracle plugin; this patch fixes an issue where the image_mapping_final.json file could have missing image_name (and other) attributes for images even though they appeared in the uuid_image_mapping.json file.

--- external_plugins/image_generating_plugin/image_generating_plugin.py
+++ external_plugins/image_generating_plugin/image_generating_plugin.py
@@ -72,23 +72,29 @@ def load_ground_truth():
         logger.error(f"An error occurred: {e}")
     return ground_truth_data
 
-def oracle_monitoring_info(track_image_count, image_uuid, image_name):
+
+def oracle_monitoring_info(track_image_count, image_uuid, image_name, ground_truth):
     """
-    Creates a file named image_mapping.json that stores information about the image file.
+    This function is called by get_binary for each image processed in the main loop.It 
+    creates and/or updates the uuid_image_mapping.json file that stores information about the image file.
+    
     Args:
         track_image_count (int): The count of image.
         uuid (str): The unique identifier for the image.
         uuid_image (str): The original name of the image file.
-        ground_truth(global dictionary): Contains ground truth information for the object in the image.
+        ground_truth: Contains ground truth information for the object in the image.
     """
     logger.info("Creating mapping file in image generating plugin")
     OUTPUT_DIR = os.environ.get('TRAPS_MAPPING_OUTPUT_PATH', "/output/")
-    file_name = "uuid_image_mapping.json"
-    image_mapping_file = os.path.join(OUTPUT_DIR, file_name)
+
+    # look up the image in the ground_truth dictionary
     ground_truth_info = ground_truth.get(image_name, 'unavailable')
-    # A value of 'unavailable' is unexpected, as it means there is a missing entry in the dictionary
+    # A value of 'unavailable' is unexpected, as it means there is a missing entry in the dictionary, 
+    # but we do not let that stop the entire program, we just log it as an error:
     if ground_truth_info == 'unavailable':
-        logger.error(f"ERROR: Could not determine ground truth value for image {file_name}; no entry in ground_truth dict.")
+        logger.error(f"ERROR: Could not determine ground truth value for image {image_name}; no entry in ground_truth dict.")
+    
+    # build the mapping dictionary for this image
     image_mapping_dict = {
         "image_count": track_image_count,
         "UUID": image_uuid,
@@ -96,7 +102,10 @@ def oracle_monitoring_info(track_image_count, image_uuid, image_name):
         "ground_truth": ground_truth_info,
         "model_id": model_variant,
     }
+    
+    # Read the uuid image mapping file to build the current map:
     mapping = {}
+    image_mapping_file = os.path.join(OUTPUT_DIR, "uuid_image_mapping.json")
     if os.path.exists(image_mapping_file):
         with open(image_mapping_file, 'r') as file:
             try:
@@ -104,26 +113,35 @@ def oracle_monitoring_info(track_image_count, image_uuid, image_name):
             except json.JSONDecodeError as e:
                 logger.error("Failed to decode JSON, exception:", e)
    
+    # Insert the new image into the map
     mapping[image_uuid] = image_mapping_dict
+    
+    # Write the updated map back to the file
     with open(image_mapping_file, 'w') as file:
         json.dump(mapping, file, indent=2)
 
-def get_binary(file_name,binary_img,img_format,track_image_count,total_images):
+
+def get_binary(file_name, binary_img, img_format, track_image_count, total_images, ground_truth):
     """
     This function is used to generate the uuid, image format and binary image and invokes the  
     new image event.
     """
-    logger.info(f"Processing file {track_image_count} of {total_images}")
+    logger.info(f"Processing file {file_name} ({track_image_count} of {total_images})")
     image_uuid = str(uuid.uuid5(uuid.NAMESPACE_URL, file_name))
     logger.info(f"Sending new image with the following data: image:{file_name}; uuid:{image_uuid}; format: {img_format}; type(format): {type(img_format)}")
     try: 
-        oracle_monitoring_info(track_image_count,image_uuid,file_name)
+        # first update the uuid_image_mapping.json file
+        oracle_monitoring_info(track_image_count, image_uuid, file_name, ground_truth)
+        
+        # then send the new image event; this should ensure that the mapping file always contains the 
+        # the image prior to a subsequent plugin processing it. 
         ctevents.send_new_image_fb_event(socket, image_uuid, img_format, binary_img)     
     except Exception as e:
         logger.error(f"ERROR: got exception {e}")
     if track_image_count>=total_images:
         logger.info(f"Succesfully processed {track_image_count} images and inititated terminating plugin from image generating plugin")
         send_terminating_plugin_fb_event(socket,"ext_image_gen_plugin","d3266646-41ec-11ed-a96f-5391348bab46")
+
 
 def monitor_generating_power():
     """
@@ -137,7 +155,8 @@ def monitor_generating_power():
         ctevents.send_monitor_power_start_fb_event(socket, pid, monitor_type, monitor_seconds)
         logger.info(f"Monitoring image generating power")
 
-def extract_from_zipfile(url):
+
+def extract_from_zipfile(url, ground_truth):
     """
     This function helps to extract images from input url.
     """
@@ -154,37 +173,47 @@ def extract_from_zipfile(url):
                         binary_img = image_file.read()
                         img = Image.open(BytesIO(binary_img))
                         img_format = img.format
-                        get_binary(file_name,binary_img,img_format,track_image_count,total_images)
+                        get_binary(file_name, binary_img, img_format, track_image_count, total_images, ground_truth)
         logger.info(f"Successfully extracted images from the url - {url}")
     else:
         logger.error(f"Failed to download file from {url}")
 
-def process_image(input_image_path):
+
+def process_images(input_image_path, ground_truth):
     """
     Main function that loops through the set of images and processes each one by ultimately sending a 
     new image event for each. The actual sending of the event happens in get_binary.
     """
     logger.info(f"The input image path specified by the user:{input_image_path}")
     if input_image_path.endswith(('.zip', '.rar')):
-        extract_from_zipfile(input_image_path)
+        extract_from_zipfile(input_image_path, ground_truth)
     track_image_count = 0
+    # NOTE: this just counts the total number of files (not including subdirectories) in the path; later, we check for the
+    #       file extension and only process supported types, so this could be an issue.
     total_images = len([name for name in os.listdir(input_image_path) if os.path.isfile(os.path.join(input_image_path, name))])
+    
+    # Iterate through the input image directory and process each file of a supported extension.
     for image_file in os.listdir(input_image_path):
-        file_name = os.path.join(input_image_path,image_file)
+        file_name = os.path.join(input_image_path, image_file)
+        
+        # check if the file is supported
         if file_name.lower().endswith(('.png', '.jpg', '.jpeg')):
-            track_image_count+=1
+            track_image_count += 1
+            # read the binary contents of the file and determine the format officially
             with open(file_name, "rb") as f:
-                binary_img = f.read()
+                binary_img = f.read()            
             img = Image.open(file_name)
             img_format = img.format
-            get_binary(file_name,binary_img,img_format,track_image_count,total_images)
+            # get_binary does the main processing for each image
+            get_binary(file_name, binary_img, img_format, track_image_count, total_images, ground_truth)
+
 
 def main():
-    global ground_truth,socket
+    global socket
     socket = get_socket()
     ground_truth = load_ground_truth()
     monitor_generating_power()
-    process_image(input_image_path)
+    process_images(input_image_path, ground_truth)
     send_quit_command(socket)
 
 if __name__ == '__main__':
